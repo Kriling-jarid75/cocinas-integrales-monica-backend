53c93696acd7716fe139ba8ebcbe2e97a5799e7d
Kriling Jarid Garcia Espinoza <kriling75@gmail.com>
2025-10-26 00:16:11 -0600

    Se sube el backend de nuevo listo para el deploy

--- src/main/java/com/cocinas/integrales/negocio/NegocioApplication.java
+++ src/main/java/com/cocinas/integrales/negocio/NegocioApplication.java
@@ -2,6 +2,10 @@ package com.cocinas.integrales.negocio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication(scanBasePackages = "com.cocinas.integrales.negocio")
@@ -12,6 +16,20 @@ public class NegocioApplication {
 		System.out.println("El backend esta corriendo......");
 	}
 	
+	
+	@Configuration
+	public static class Myconfiguration{
+		@Bean
+		public WebMvcConfigurer corsConfigurer(){
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+				}
+			};
+		}
+	}
 	
 	
 
